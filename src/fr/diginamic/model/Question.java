8801de1f194cc642039f2b44464214ad42fa3129
Amaury <amaurygranvillain17@gmail.com>
2018-11-14 16:40:28 +0100

    Création des méthodes add et vérif réponses

--- src/fr/diginamic/model/Question.java
+++ src/fr/diginamic/model/Question.java
@@ -5,4 +5,19 @@ public class Question {
 	String intitule;
 	String[] propositions;
 	String bonneReponse;
+	
+	public Question (String intitule, int nbreponse) {
+		this.intitule = intitule;
+		this.propositions = new String[nbreponse];
+	}
+	public boolean verifierReponse(String verifReponse) {
+		return verifReponse.equals(bonneReponse);
+	}
+	public void addProposition(String nouvelleproposition) {
+		for (int i = 0; i < propositions.length; i++) {
+			if(propositions[i] == null) {
+				propositions[i] = nouvelleproposition;
+			}
+		}
+	}
 }
